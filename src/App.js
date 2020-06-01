0b3691edead0b1f482f88d3e9740d21b9fd92d28
karsa <wanghongtao114@gmail.com>
2020-06-01 15:50:10 +0800

    remove old ui

--- src/App.js
+++ src/App.js
@@ -1,75 +1,29 @@
 import React from 'react';
-import {NovelList} from './NovelList';
-import  {NovelSummary} from './NovelSummary';
-import {NovelDetail} from './NovelDetail';
-
+import {reqNovelList, reqChapterList, reqChapterDetail} from './novelRequest';
 class App extends React.Component {  
   constructor(props) {
-    super(props)    
-    let router = JSON.parse(localStorage.getItem("router"));
-    if (!router) {
-      router = [];
-    }
-    this.state = {router}
-  }
-  updateRouter() {
-    if (this.state.router) {
-      localStorage.setItem("router", JSON.stringify(this.state.router))    
-    } else {
-      localStorage.setItem("router", JSON.stringify([]))    
-    }
-    console.log(this.state.router)
-  }
-  showPage(key, id, otherInfo) {    
-    let router = this.state.router;
-    router.push({
-      key, id, otherInfo
-    })
-    this.setState({router}, () => {
-      this.updateRouter()
-    })    
-  }
-
-  back() {
-    let router = this.state.router;
-    if (this.state.router.length > 0) {
-      router = router.filter((_, i) => i < this.state.router.length-1)
-    }
-    this.setState({router}, () => {
-      this.updateRouter()
-    })    
-  }
-
-  scrollToTop() {
-    if (this.topRef) {
-      this.topRef.scrollIntoView()
-    }
-  }
-
-  render() {    
-    let item = <NovelList scrollToTop={this.scrollToTop.bind(this)} showPage={this.showPage.bind(this)}  />
-    if (this.state.router.length > 0) {
-      let itemMap =  this.state.router[this.state.router.length-1] ;
-      switch (itemMap.key) {
-        case 'NovelSummary':
-          item = <NovelSummary scrollToTop={this.scrollToTop.bind(this)} showPage={this.showPage.bind(this)} back={() => {
-            this.back()
-          }} id={itemMap.id} />
-          break
-        case "NovelDetail":
-          item = <NovelDetail scrollToTop={this.scrollToTop.bind(this)} showPage={this.showPage.bind(this)} back={() => {
-            this.back()
-          }} id={itemMap.id} otherInfo={itemMap.otherInfo} />
-          break
-        default:
-          break
+    super(props)        
+    reqNovelList(0, 10).then(res => {
+      console.log(res.total)
+      console.log(res.list)
+      if (res.list && res.list.length > 0) {        
+        let novel = res.list[0];
+        reqChapterList(novel.id, 0, 10).then(res => {
+          console.log(res.total)
+          console.log(res.list)
+          if (res.list && res.list.length > 0) {
+            let chapter = res.list[0];
+            reqChapterDetail(chapter.id).then(res => {
+              console.log(res);
+            })
+          }          
+        })
       }      
-    }
+    })
+  }
+  render() {
     return (
-      <div>
-        <div ref={(el) => { this.topRef = el; }} />
-        {item}
-      </div>
+      <div></div>
     )
   }
 }
--- src/novelRequest.js
+++ src/novelRequest.js
@@ -1,12 +1,38 @@
-export function GetRequest(url, parameter) {
+const envs = {  
+  normal : {
+    baseUrl: 'http://47.103.193.221/novel-api/',
+  },
+  dev : {
+    baseUrl: 'http://localhost:8020/',
+  }
+}
+
+const env = envs.dev;
+
+function getRequest(url, parameter) {
   let bodyStr = ""
   if (parameter) {
     bodyStr = JSON.stringify(parameter)
   }
-  return fetch("http://47.103.193.221/novel-api/" + url, {
+  return fetch(env.baseUrl + url, {
     body: bodyStr,
     method: 'POST',
   })
     .then(res => res.json())
     .then(res => res.data)    
 }
+
+export function reqNovelList(page, size) {
+  return getRequest("novel/list", {page, size})
+}
+
+export function reqChapterList(novelId, page, size) {
+  return getRequest("novel/chapters/"+novelId, {page, size})
+}
+
+export function reqChapterDetail(chapterId) {
+  return getRequest('chapter/detail/' + chapterId)
+}
+
+
+
